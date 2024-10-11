d582f6b0cafcb382c5fef00c9718cfc23ba69661
JuandaSilvaa <almeidajuan529@gmail.com>
2024-10-11 00:30:49 -0300

    Projeto base finalizado

---
+++ src/main/java/br/org/serratec/biblioteca/domain/Livro.java
@@ -0,0 +1,64 @@
+package br.org.serratec.biblioteca.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table
+public class Livro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Preencha o titulo")
+	@Column(nullable = false)
+	private String titulo;
+
+	@Column
+	private int qtdPaginas;
+
+	@Embedded
+	@Valid
+	private Publicacao publicacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getQtdPaginas() {
+		return qtdPaginas;
+	}
+
+	public void setQtdPaginas(int qtdPaginas) {
+		this.qtdPaginas = qtdPaginas;
+	}
+
+	public Publicacao getPublicacao() {
+		return publicacao;
+	}
+
+	public void setPublicacao(Publicacao publicacao) {
+		this.publicacao = publicacao;
+	}
+
+}
